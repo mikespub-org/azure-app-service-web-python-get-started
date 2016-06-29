06de402e211e12011e332b738f878940a8fefcd7
Mike's Pub <mikespub@users.noreply.github.com>
2016-06-30 00:25:47 +0200

    ugly work-around to serve static files on Azure

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
 # from django.contrib import admin
@@ -40,4 +43,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
